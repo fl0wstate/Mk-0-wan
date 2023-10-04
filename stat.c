da59feca674355a1f078450ffdbf0b0871c6497d
Mk-0-wan <victormurithi2020@gmail.com>
2023-10-04 11:10:54 +0300

    simple file stat check

--- stat.c
+++ stat.c
@@ -3,6 +3,7 @@
 #include <sys/types.h>
 #include <fcntl.h>
 #include <sys/stat.h>
+#include <sys/sysmacros.h>
 
 int main(int ac, char **av) /* argument vector */
 {
@@ -18,6 +19,22 @@ int main(int ac, char **av) /* argument vector */
 	{
 		printf("Inode numbern: %d", file_info_stat.st_ino);
 		printf("Access mode: %lu", file_info_stat.st_mode);
+		printf("ID of containing device:  [%x,%x]\n",
+                  major(file_info_stat.st_dev),
+                  minor(file_info_stat.st_dev));
+
+           	printf("File type:                ");
+           	switch (file_info_stat.st_mode & S_IFMT)
+		{
+			case S_IFBLK:  printf("block device\n");            break;
+			case S_IFCHR:  printf("character device\n");        break;
+			case S_IFDIR:  printf("directory\n");               break;
+			case S_IFIFO:  printf("FIFO/pipe\n");               break;
+			case S_IFLNK:  printf("symlink\n");                 break;
+			case S_IFREG:  printf("regular file\n");            break;
+			case S_IFSOCK: printf("socket\n");                  break;
+			default:       printf("unknown?\n");                break;
+		}
 	}
 	else
 	{
